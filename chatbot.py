b12a150df597530b9456ce0fc8e6569dcb187a43
Tanmay Bhatnagar <tanmaybhatnagar84976@gmail.com>
2018-08-19 04:36:15 +0530

    Replaced abbreviated words

--- chatbot.py
+++ chatbot.py
@@ -34,3 +34,28 @@ for conversation in conversations_ids:
         for i in range(len(conversation ) - 1):
             questions.append(id2line[conversation[i]])
             answers.append(id2line[conversation[i+1]])
+            
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm" , "i am" , text)
+    text = re.sub(r"he's" , "he is" , text)
+    text = re.sub(r"she's" , "she is" , text)
+    text = re.sub(r"that's" , "that is" , text)
+    text = re.sub(r"where's" , "where is" , text)
+    text = re.sub(r"i'm" , "i am" , text)
+    text = re.sub(r"\'ll" , "will" , text)
+    text = re.sub(r"\'ve" , "have" , text)
+    text = re.sub(r"\'re" , "are" , text)
+    text = re.sub(r"\'d" , "would" , text)
+    text = re.sub(r"won't" , "will not" , text)
+    text = re.sub(r"can't" , "cannot" , text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]" . "" . text)
+    return text
+    
+    
+    
+    
+    
+    
+    
+    
