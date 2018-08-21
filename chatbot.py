77740adfe7251e6a0f019283dfcc273d72ad79cf
Tanmay Bhatnagar <tanmaybhatnagar84976@gmail.com>
2018-08-21 07:42:16 +0530

    Added EOS token to answers and mappend Q&A to integers

--- chatbot.py
+++ chatbot.py
@@ -45,7 +45,8 @@ def clean_text(text):
     text = re.sub(r"where's" , "where is" , text)
     text = re.sub(r"i'm" , "i am" , text)
     text = re.sub(r"\'ll" , " will" , text)
-    text = re.sub(r"\'ve" , " have" , text)
+    text = re.sub(r"\'ve" , " have" , text)Back-to-School Sale
+Learn your way with courses from ₹640! The season's best sale starts now
     text = re.sub(r"\'s" , " is" , text)
     text = re.sub(r"\'re" , " are" , text)
     text = re.sub(r"\'d" , " would" , text)
@@ -99,4 +100,41 @@ for word, count in word2count.items():
         answerswords2int[word] = word_number
         word_number += 1
         
+#Adding last tokens to both dictionaries
+tokens = ['<PAD>' , '<EOS>', '<OUT>' , '<SOS>']
+for token in tokens:
+        questionswords2int[token] = len(questionswords2int) + 1
+for token in tokens:
+        answerswords2int[token] = len(answerswords2int) + 1
+
+#Inverse mapping of answerswords2int
+answersints2word = {w_i : w for w , w_i in answerswords2int.items()}
+
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>'
     
+#Translating questions and answers to their integer counterpart
+#Replacing non frequent words with <OUT>
+
+questions_to_int = []
+answers_to_int = []
+
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionswords2int:
+            ints.append(questionswords2int['<OUT>'])
+        else:
+            ints.append(questionswords2int[word])
+    questions_to_int.append(ints)
+
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerswords2int:
+            ints.append(answerswords2int['<OUT>'])
+        else:
+            ints.append(answerswords2int[word])
+    answers_to_int.append(ints)
+
+
