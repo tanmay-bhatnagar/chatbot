452075490a411d9ed67fe1662dd451a3d4e02067
Tanmay Bhatnagar <tanmaybhatnagar84976@gmail.com>
2018-08-23 00:23:20 +0530

    Preprocessed input tensors, added SOS to answers

--- chatbot.py
+++ chatbot.py
@@ -156,5 +156,12 @@ def model_inputs():
     lr = tf.placeholder(tf.float32,name = 'learning_rate')
     keep_prob = tf.placeholder(tf.float32 , name = 'keep_prob') #For dropout
     return inputs , targets , lr , keep_prob
+
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size , 1] , word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0] , [batch_size , -1], [1,1] )
+    preprocessed_targets = tf.concat([left_side,right_side] , 1)
+    return preprocessed_targets
+a
     
     
