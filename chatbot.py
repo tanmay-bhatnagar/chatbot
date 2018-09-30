5ab49df1abc2107883a52d8da30d2b300df71df8
Tanmay Bhatnagar <tanmaybhatnagar84976@gmail.com>
2018-09-30 23:48:53 +0530

    Added loop to encode question before chat

--- chatbot.py
+++ chatbot.py
@@ -416,3 +416,8 @@ session = tf.InteractiveSession()
 session.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 saver.restore(session, checkpoint)
+
+# Converting the questions from strings to lists of encoding integers
+def convert_string2int(question, word2int):
+    question = clean_text(question)
+    return [word2int.get(word, word2int['<OUT>']) for word in question.split()]
